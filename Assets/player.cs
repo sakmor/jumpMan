84f62d72b4766b9ec9db39e92585224bef38a455
王山姆 <sakmor@gmail.com>
2017-03-07 23:29:26 +0800

    整理code 並新增加速跑

--- Assets/player.cs
+++ Assets/player.cs
@@ -7,10 +7,12 @@ public class player : MonoBehaviour
 {
     public bool IsGrounded;
     public int score;
+    public float speedUP;
     GameObject[] allBlock;
     // Use this for initialization
     void Start()
     {
+        speedUP = 1;
         allBlock = GameObject.FindGameObjectsWithTag("block");
         IsGrounded = true;
     }
@@ -20,26 +22,78 @@ public class player : MonoBehaviour
     {
         jumpThroughHead();
         animatorState();
+    }
+    public void jump()
+    {
+        if (IsGrounded)
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 500);
+    }
+
+    public void fall()
+    {
+        this.GetComponent<Rigidbody>().velocity *= 0.5f;
 
     }
+    public void right()
+    {
+        this.GetComponent<Rigidbody>().position += Vector3.right * 0.15f * speedUP;
+        if (IsGrounded)
+        {
+            this.GetComponent<Animator>().SetInteger("state", 1);
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+    public void pressA()
+    {
+        if (IsGrounded)
+        {
+            speedUP = 2f;
+        }
+
+    }
+    public void releaseA()
+    {
+        if (IsGrounded)
+        {
+            speedUP = 1f;
+        }
+
+    }
+    public void left()
+    {
+        this.GetComponent<Rigidbody>().position += Vector3.left * 0.15f * speedUP;
+        if (this.GetComponent<player>().IsGrounded)
+        {
+            this.GetComponent<Animator>().SetInteger("state", 1);
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+    }
+    //animatorState: 依據player的各種狀態，判斷現在對應的動畫狀態
     void animatorState()
     {
         if (!IsGrounded)
         {
+            //如果在空中，且正在往上升:2-jump
             if (GetComponent<Rigidbody>().velocity.y > 0)
             {
                 GetComponent<Animator>().SetInteger("state", 2);
             }
             else
+            //如果在空中，且正在往下降:3-fall
             {
                 GetComponent<Animator>().SetInteger("state", 3);
             }
         }
+
         if (IsGrounded)
         {
+            //如果在地面:0-idel
             GetComponent<Animator>().SetInteger("state", 0);
         }
     }
+
+    //讓player可以從下往上穿過block的判斷式
     void jumpThroughHead()
     {
         if (!IsGrounded)
@@ -66,6 +120,8 @@ public class player : MonoBehaviour
             }
         }
     }
+
+    //OnCollisionEnter:當player碰到其他collision時(一次性)...
     void OnCollisionEnter(Collision collision)
     {
         //碰到star時
@@ -83,6 +139,7 @@ public class player : MonoBehaviour
         }
     }
 
+    //OnCollisionEnter:當player持續碰到其他collision時(重複性)...
     void OnCollisionStay(Collision collision)
     {
         //碰到block時
@@ -92,7 +149,7 @@ public class player : MonoBehaviour
         }
     }
 
-
+    //OnCollisionEnter:當player離開碰到的collision時(一次性)...
     void OnCollisionExit(Collision collision)
     {
         //當離開block時
